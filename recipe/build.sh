5d6d10db894614da1a58d12e9438426ebcf302ab
Samuel Debionne <samuel.debionne@esrf.fr>
2020-12-23 11:31:31 +0100

    Fix build for v2.0.13

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,8 +15,7 @@ fi
 cmake \
     -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH="${PREFIX}" \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}/include" \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DBUILD_DOCS=0 \
     -DBUILD_EXAMPLES=0 \
     -DBUILD_TESTS=0 \
